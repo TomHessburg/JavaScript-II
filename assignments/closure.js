5512ecc10044ca6bfbec55aa3c5aaddef9096711
Tom Hessburg <tomhessburg@Toms-MacBook-Pro.local>
2019-01-22 17:09:59 -0500

    closure stretch complete... i THINK i did this in an acceptable manner, but i did modify the function to just be an object so im not exactly sure that its perfect, though Im close to certain that this is what the instructions are trying to say to do...

--- assignments/closure.js
+++ assignments/closure.js
@@ -62,12 +62,23 @@ counter();
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+const counterFactory = {
+  
+  myNumber: 0,
+  increment: function(){
+    this.myNumber++;
+    return this.myNumber;
+  },
+  decrement: function(){
+    this.myNumber--;
+    return this.myNumber;
+  }
 };
 
+console.log(counterFactory.myNumber);
+console.log(counterFactory.increment());
+console.log(counterFactory.decrement());
+console.log(counterFactory.decrement());
 
 
 console.log("===END CLOSURES ");
